895ed7d353b631bf91e306a8ba817702344919f6
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-29 16:42:12 +1000

    question and options are loading on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,52 @@ var questionsCount;
 var correctAnswer;
 
 var triviaQuestions = {
-    "question" : "The trivia question",
-    "answer" : "The correct answer",
-    "options" : ["option number 1", "option number 2", "The correct answer", "option number 4"],
+    "question" : ["The trivia question", "the second question"],
+    "answer" : ["The correct answer", "the second answer"],
+    "options" : [["option number 1", "option number 2", "The correct answer", "option number 4"],["option number 1", "option number 2", "The correct answer", "option number 4"]]
 }
 
+var answerA;
+var answerB;
+var answerC;
+var answerD;
+
+//On load or reset
+function startGame(){
+    answeredCorrect = 0
+    answeredWrong = 0
+    questionsCount = 0
+}
+
+$(document).ready(function() {
+
+//Set the question
+function setQuestion(){
+    $(".questionText").text(triviaQuestions.question[questionsCount]);
+    correctAnswer = triviaQuestions.answer[questionsCount];
+
+    answerA = triviaQuestions.options[questionsCount][0];
+    $("#answerAText").text(answerA)
+
+    answerB = triviaQuestions.options[questionsCount][1];
+    $("#answerBText").text(answerB)
+
+    answerC = triviaQuestions.options[questionsCount][2];
+    $("#answerCText").text(answerC)
+
+    answerD = triviaQuestions.options[questionsCount][3];
+    $("#answerDText").text(answerD)
+    }
+        
+//Picking the correct answer
+function answerIsCorrect(){
+    answeredCorrect++
+    questionsCount++
+    
+}
+
+startGame()
+setQuestion()
+
+
+});
