6e369cdfc42915e6eecd54aba7b650468f2c42ef
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-29 16:08:21 +1000

    starting on the js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var answeredCorrect;
+var answeredWrong;
+var questionsCount;
+
+var correctAnswer;
+
+var triviaQuestions = {
+    "question" : "The trivia question",
+    "answer" : "The correct answer",
+    "options" : ["option number 1", "option number 2", "The correct answer", "option number 4"],
+}
+
