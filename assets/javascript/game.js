30d29ba166294c861b60abacc43fae412015a5c8
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-30 16:45:41 +1000

    changed the trivia questions into a better array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,20 @@ var questionsCount;
 
 var correctAnswer;
 
-var triviaQuestions = {
-    "question" : ["The trivia question", "the second question"],
-    "answer" : ["The correct answer", "the second answer"],
-    "options" : [["option number 1", "option number 2", "The correct answer", "option number 4"],["option number 1", "option number 2", "The correct answer", "option number 4"]]
-}
+var triviaQuestions = [
+    questionOne = {
+    "question" : "The trivia question",
+    "answer" : "The correct answer",
+    "options" : ["option number 1", "option number 2", "The correct answer", "option number 4"]
+    },
+
+    questionTwo = {
+        "question" : "The 2nd trivia question",
+        "answer" : "The 2nd correct answer",
+        "options" : ["option 2 number 1", "option 2nd number 2", "The 2nd correct answer", "option 2 number 4"]
+        }
+
+    ]
 
 var answerA;
 var answerB;
@@ -16,30 +25,33 @@ var answerC;
 var answerD;
 var selectedAnswer;
 
+var rounds;
+
 //On load or reset
 function startGame(){
-    answeredCorrect = 0
-    answeredWrong = 0
-    questionsCount = 0
+    answeredCorrect = 0;
+    answeredWrong = 0;
+    questionsCount = 0;
+    rounds = 0;
 }
 
 $(document).ready(function() {
 
 //Set the question
 function setQuestion(){
-    $(".questionText").text(triviaQuestions.question[questionsCount]);
-    correctAnswer = triviaQuestions.answer[questionsCount];
+    $(".questionText").text(triviaQuestions[rounds].question);
+    correctAnswer = triviaQuestions[rounds].answer;
 
-    answerA = triviaQuestions.options[questionsCount][0];
+    answerA = triviaQuestions[rounds].options[0];
     $("#answerAText").text(answerA)
 
-    answerB = triviaQuestions.options[questionsCount][1];
+    answerB = triviaQuestions[rounds].options[1];
     $("#answerBText").text(answerB)
 
-    answerC = triviaQuestions.options[questionsCount][2];
+    answerC = triviaQuestions[rounds].options[2];
     $("#answerCText").text(answerC)
 
-    answerD = triviaQuestions.options[questionsCount][3];
+    answerD = triviaQuestions[rounds].options[3];
     $("#answerDText").text(answerD)
     }
         
@@ -60,7 +72,7 @@ function answerIsWrong(){
 function scorecardUpdate(){
     $("#correctAnswerText").text("Correct Answers: " + answeredCorrect);
     $("#wrongAnswerText").text("Wrong Answers: " + answeredWrong);
-    $("#questionsRemainingText").text("Questions remaining: " + questionsCount);
+    $("#questionsRemainingText").text("Questions remaining: " + (triviaQuestions.length - questionsCount));
 }
 
 startGame()
@@ -78,6 +90,8 @@ $("button").on("click", function(){
     }
     
     scorecardUpdate();
+    rounds++
+    setQuestion();
     
 })
 
